be4d5c2fba4d2c7a052f7944334c1f3d83896ade
parsa <akbariparsa1209@gmail.com>
2020-09-06 10:29:48 +0430

    Calling SendReq and showing the result.

--- client/application/main.go
+++ client/application/main.go
@@ -5,8 +5,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/parsaakbari1209/Database-Security-Proxy/client/application/app"
 )
 
+// Main func is a shell like programme running in a terminal.
+// It gets a connection string and a query and sends them to SendReq func in app/app.go
+// Then gets the result and prints in them.
+// These steps are looped forever until the proccess is killed.
 func main() {
 	fmt.Println("started client command-line application...")
 	fmt.Println("")
@@ -23,6 +29,12 @@ func main() {
 		query, _ := reader.ReadString('\n')
 		query = strings.Replace(query, "\n", "", -1)
 
+		fmt.Println("")
+		fmt.Println("---------- RESULT ----------")
+		res := app.SendReq(connString, query)
+		for _, row := range res {
+			fmt.Println(row[1])
+		}
 		fmt.Println("")
 	}
 }
