2e1ae21f1a02746e8a378bef6db7a39c8820b720
parsa <akbariparsa1209@gmail.com>
2020-09-08 12:05:38 +0430

    Added different os support (TODOs).

--- client/application/main.go
+++ client/application/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"runtime"
 	"strings"
 
 	"github.com/parsaakbari1209/Database-Security-Proxy/client/application/app"
@@ -17,11 +19,11 @@ var (
 	c tlspb.TLSClientServiceClient
 )
 
-// TODO: the stream functionality must be implemented through a pipe!
 // main func is a shell like programme running in a terminal.
 // First, connects to the client-side-tls-service server.
-// Second, gets a connection string and a query and sends them to SendReq func with c.
-// Third, gets the result and prints them.
+// Second, gets a connection string and a query.
+// Third, calls SendReq func with connection-string, query and connection-iteself.
+// Fourth, gets the result(stream) and prints them to standard-output.
 // These steps are looped forever until the proccess is killed.
 func main() {
 	fmt.Println("started client command-line application...")
@@ -30,7 +32,6 @@ func main() {
 	// Reader for reading lines from terminal.
 	reader := bufio.NewReader(os.Stdin)
 
-	// TODO: Implemetent secure connection!
 	// Dial to the client-side-tls-service server.
 	address := "localhost:50051"
 	cc, err := grpc.Dial(address, grpc.WithInsecure())
@@ -40,27 +41,38 @@ func main() {
 	defer cc.Close()
 	c = tlspb.NewTLSClientServiceClient(cc)
 
+	// Get newline char based on the OS.
+	newline := newline()
 	// Loop forever for shell like functionality.
 	for {
-		// TODO: Newlines in windows are \r\n so, this won't work on windows.
 		// Get connection string.
 		fmt.Print("con str -> ")
 		connString, _ := reader.ReadString('\n')
-		connString = strings.Replace(connString, "\n", "", -1)
+		connString = strings.Replace(connString, newline, "", -1)
 
-		// TODO: Newlines in windows are \r\n so, this won't work on windows.
 		// Get query.
 		fmt.Print("sql str -> ")
 		query, _ := reader.ReadString('\n')
-		query = strings.Replace(query, "\n", "", -1)
+		query = strings.Replace(query, newline, "", -1)
 
 		// Send request and show response.
 		fmt.Println("")
 		fmt.Println("---------- RESULT ----------")
-		res := app.SendReq(connString, query, c)
-		for _, row := range res {
-			fmt.Println(row[1])
+		r := app.SendReq(connString, query, c)
+		if _, err := io.Copy(os.Stdout, r); err != nil {
+			log.Fatalf("error while writing to standard-output: %v", r)
 		}
 		fmt.Println("")
 	}
+}
+
+// newline func returns a newline character based on the os.
+// For windows OSes: \r\n
+// For unix-like OSes: \n
+func newline() string {
+	newline := "\n"
+	if runtime.GOOS == "windows" {
+		newline = "\r\n"
+	}
+	return newline
 }
